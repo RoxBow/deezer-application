f7c74416fc1be425a4e93f0d8bf11e924293e99a
Deplais Vincent <vincent.deplais@orange.fr>
2022-05-26 00:21:14 +0200

    Clean & setup query search

---
+++ pages/api/search.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { EXTERNAL_API_URL } from '@constants';
+
+type QueryParameters = {
+  term: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { term } = req.query as QueryParameters;
+
+  const data = await axios
+    .get(`${EXTERNAL_API_URL}/search?q=track:"${term}"`)
+    .then((res) => res.data);
+
+  res.status(200).json({ data });
+}
