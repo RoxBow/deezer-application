3211ea9bf17858cb129613bf89fc96ed617cc985
Deplais Vincent <vincent.deplais@orange.fr>
2022-05-26 00:20:23 +0200

    Add base config for SWR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { SWRConfig } from 'swr';
+import fetcher from '@utils/fetcher';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SWRConfig value={{ fetcher }}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SWRConfig>
   );
 }
 
