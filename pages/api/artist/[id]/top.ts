d5545eb5df38e9b5e04208051d627446d309973a
Deplais Vincent <vincent.deplais@orange.fr>
2022-05-26 02:53:48 +0200

    Setup API for artist

---
+++ pages/api/artist/[id]/top.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { EXTERNAL_API_URL } from '@constants';
+
+type QueryParameters = {
+  id: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query as QueryParameters;
+
+  const data = await axios
+    .get(`${EXTERNAL_API_URL}/artist/${id}/top`)
+    .then((res) => res.data);
+
+  res.status(200).json({ tops: data?.data || [] });
+}
